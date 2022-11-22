1bcadbff001e043d3c8c5f73c61d2eec83e78806
ikromyusupov <@ikromyusupovdev@gmail.com>
2022-11-22 20:06:52 +0500

    added test_cases

---
+++ test_cases.py
@@ -0,0 +1,14 @@
+## count_jobs
+'''**Example 1:**
+
+```Python
+Input: ([{'name': 'John', 'job': 'Developer'}, {'name': 'Mary', 'job': 'Developer'}],"Developer")
+Output: 2
+
+```
+**Example 2:**
+
+```Python
+Input: ([{'name': 'John', 'job': 'Barber'}, {'name': 'Mary', 'job': 'Developer'},{'name': 'Ann', 'job': 'Teacher'}],"Student")
+Output: 0
+```'''
