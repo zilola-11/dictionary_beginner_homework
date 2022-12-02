b54dcc866fbeef99604598cfafc847fcec535755
ikromyusupov <@ikromyusupovdev@gmail.com>
2022-12-02 15:13:27 +0500

    updated

--- find_max_key.py
+++ find_max_key.py
@@ -1,6 +1,6 @@
 def find_max_key(data: dict) -> int:
     """
-    Return the maximum key in a dictionary.
+    Return the maximum int key in a dictionary.
     Args:
         data (dict): A dictionary of values
     Returns:
