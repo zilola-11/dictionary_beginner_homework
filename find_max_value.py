493d05be465f3e788ec4ee25e3777cb2b24af8e4
ikromyusupov <@ikromyusupovdev@gmail.com>
2022-12-02 15:24:25 +0500

    updated

--- find_max_value.py
+++ find_max_value.py
@@ -1,6 +1,6 @@
 def find_max_value(data: dict) -> int:
     """
-    Return the maximum int value in a dictionary.
+    Return the maximum int or float value in a dictionary.
     Args:
         data (dict): A dictionary of values
     Returns:
