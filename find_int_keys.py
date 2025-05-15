72869d58da38e1780f42180dbd978a529904c92d
zilola-11 <asadbekpodsho@gmail.com>
2025-05-16 01:52:14 +0500

    duet

--- find_int_keys.py
+++ find_int_keys.py
@@ -6,4 +6,10 @@ def find_int_keys(data: dict) -> list:
     Returns:
         list: A list of all keys in the dictionary that are integers.
     """
-    return 
+    key_list = []
+    for key in data:
+        if type(key) == int:
+            key_list.append(key)
+    print(key_list)
+find_int_keys({1: "one", 2: "two", "three": 3, 4.0: "four"})
+print(find_int_keys({1: "one", 2: "two", "three": 3, 4.0: "four"}))
